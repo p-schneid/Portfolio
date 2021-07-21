bda298d152682dd5ab5ede5ce984f3462c76547f
Paul Schneider <paul.schneider4@gmail.com>
2021-07-21 16:58:33 -0400

    added software page template

--- index.js
+++ index.js
@@ -15,21 +15,26 @@ router.use(function (req,res,next) {
 });
 
 router.get("/",function(req,res){
-  res.sendFile(viewPath + "vr.html");
+  res.sendFile(viewPath + "software/software.html");
+});
+
+router.get("/software",function(req,res){
+  res.sendFile(viewPath + "software/software.html");
 });
 
 router.get("/vr",function(req,res){
-  res.sendFile(viewPath + "vr.html");
+  res.sendFile(viewPath + "vr/vr.html");
 });
 
 router.get("/design",function(req,res){
-  res.sendFile(viewPath + "design.html");
+  res.sendFile(viewPath + "design/design.html");
 });
 
 router.get("/tilt",function(req,res){
-  res.sendFile(viewPath + "tilt.html");
+  res.sendFile(viewPath + "tilt/tilt.html");
 });
 
+app.use("/views", express.static(PATH.join(__dirname, 'views')))
 app.use("/public", express.static(PATH.join(__dirname, 'public')))
 app.use("/node_modules", express.static(PATH.join(__dirname, 'node_modules')))
 
